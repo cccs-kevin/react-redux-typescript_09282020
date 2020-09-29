308812c2cfc8c3b53a50acbf8c2403a0e922a489
Eric Greene <eric@t4d.io>
2020-09-29 14:00:55 -0400

    add dynamic header

--- demo-app/src/components/CarTable.tsx
+++ demo-app/src/components/CarTable.tsx
@@ -4,6 +4,12 @@ import { Car, CarsOrder, CarKeys } from "../models/car";
 
 import { CarViewRow } from "./CarViewRow";
 
+export type CarTableCol = {
+  id: number;
+  caption: string;
+  column: CarKeys;
+};
+
 export type CarTableProps = {
   cars: Car[];
   carsOrder: CarsOrder;
@@ -11,51 +17,32 @@ export type CarTableProps = {
   onSortCars: (carProp: CarKeys) => void;
 };
 
+const carTableCols: CarTableCol[] = [
+  { id: 1, caption: "Id", column: "id" },
+  { id: 2, caption: "Make", column: "make" },
+  { id: 3, caption: "Model", column: "model" },
+  { id: 4, caption: "Year", column: "year" },
+  { id: 5, caption: "Body Color", column: "color" },
+  { id: 6, caption: "Price", column: "price" },
+];
+
 export function CarTable(props: CarTableProps) {
   return (
     <table>
       <thead>
         <tr>
-          <th onClick={() => props.onSortCars("id")}>
-            Id
-            {props.carsOrder.column === "id" &&
-              "(" + props.carsOrder.direction + ")"}
-          </th>
-          <th>
-            <button type="button" onClick={() => props.onSortCars("make")}>
-              Make
-              {props.carsOrder.column === "make" &&
-                "(" + props.carsOrder.direction + ")"}
-            </button>
-          </th>
-          <th>
-            <button type="button" onClick={() => props.onSortCars("model")}>
-              Model
-              {props.carsOrder.column === "model" &&
-                "(" + props.carsOrder.direction + ")"}
-            </button>
-          </th>
-          <th>
-            <button type="button" onClick={() => props.onSortCars("year")}>
-              Year
-              {props.carsOrder.column === "year" &&
-                "(" + props.carsOrder.direction + ")"}
-            </button>
-          </th>
-          <th>
-            <button type="button" onClick={() => props.onSortCars("color")}>
-              Color
-              {props.carsOrder.column === "color" &&
-                "(" + props.carsOrder.direction + ")"}
-            </button>
-          </th>
-          <th>
-            <button type="button" onClick={() => props.onSortCars("price")}>
-              Price
-              {props.carsOrder.column === "price" &&
-                "(" + props.carsOrder.direction + ")"}
-            </button>
-          </th>
+          {carTableCols.map((carTableCol) => (
+            <th key={carTableCol.id}>
+              <button
+                type="button"
+                onClick={() => props.onSortCars(carTableCol.column)}
+              >
+                {carTableCol.caption}
+                {props.carsOrder.column === carTableCol.column &&
+                  "(" + props.carsOrder.direction + ")"}
+              </button>
+            </th>
+          ))}
           <th>Actions</th>
         </tr>
       </thead>
